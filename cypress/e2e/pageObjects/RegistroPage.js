8790d12c5ee36cf461ba0f65e9458973be817e60
Ariadne Blanco <ariadneblanco20@gmail.com>
2025-02-14 00:19:47 -0500

    Eliminación de código no necesario

--- cypress/e2e/pageObjects/RegistroPage.js
+++ cypress/e2e/pageObjects/RegistroPage.js
@@ -16,18 +16,7 @@ class RegistroPage{
     clickSubmitButton(){
         cy.get('button[type="submit"]').click()
     }
-
-    checkDuplicateEmailError() {
-        cy.get('app-toast > .flex')
-          .should('be.visible') 
-          .then(($toast) => {
-            const message = $toast.text().trim(); // Obtén el texto del mensaje de error
-            if (message !== "Email inválido") {
-              throw new Error(`La prueba falló. El sistema permitió registrar al usuario`); 
-            }
-          })
-      }
-           
+          
     verifyMessage(mensajeEsperado){
         cy.contains(mensajeEsperado).should("be.visible")
     }
